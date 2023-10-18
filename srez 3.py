6c032e6b8f7a6aa9ea2b345c050ff9b2191d7421
DmitriNoviko <143862586+DmitriNoviko@users.noreply.github.com>
2023-10-18 18:24:50 +0700

    Add files via upload

---
+++ srez 3.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a = str(input("Введи все что угодно: "))
+print(a[0],a[1],a[2],a[3],a[4]) 
+
